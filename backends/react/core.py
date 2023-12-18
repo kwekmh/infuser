9609f175622ea00940e9c983dc219c5fc3821ac9
Kwek Ming Hong <me@kwekmh.com>
2023-12-18 13:19:45 +0800

    fix: update system prompt for ReAct

--- backends/react/core.py
+++ backends/react/core.py
@@ -21,41 +21,46 @@ class ReactBackend(BaseBackend):
 
     def query(self, model, message, max_turns=5):
         react_prompt = '''
-        You run in a loop of Thought, Action, PAUSE and Observation.
-        
-        At the end of every loop, you output an Answer.
-        
-        Use Thought to describe your thoughts about the Question you have been asked.
-        
-        Use Action to run one of the actions available to you. Return PAUSE after each Action.
-        
-        Observation is the output from each Action that will be given to you.
-        
-        The actions available to you are:
-        
-        wikipedia:
-        e.g. wikipedia: python
-        Returns a summary from looking up Wikipedia.
-        
-        Always look things up on Wikipedia if you can.
-        
-        Example session:
-        
-        Question: What is the capital city of Indonesia?
-        Thought: I should look up Indonesia on Wikipedia.
-        Action: wikipedia: Indonesia
-        PAUSE
-        
-        You will be called again with:
-        
-        Observation: Indonesia,[a] officially the Republic of Indonesia,[b] is a country in Southeast Asia and Oceania between the Indian and Pacific oceans. The country's capital, Jakarta, is the world's second-most populous urban area.
-        
-        You will then answer, if you have the answer:
-        
-        Answer: The capital city of Indonesia is Jakarta.
-        
-        If you do not have the answer, you can return an Action and return PAUSE after.
+You run in a loop of Thought, Action, PAUSE and Observation.
+
+At the end of every loop, you output an Answer.
+
+Use Thought to describe your thoughts about the Question you have been asked.
+
+Use Action to run one of the actions available to you. Return PAUSE after each Action.
+
+When you PAUSE, you do not Answer, and you wait for an Observation.
+
+Observation is the output from each Action that will be given to you.
+
+You combine Observation with your own understanding to attempt to answer the question.
+
+The actions available to you are:
+
+wikipedia:
+e.g. wikipedia: python
+Returns a summary from looking up Wikipedia.
+
+Always look things up on Wikipedia if you can.
+
+Example session:
+
+Question: What is the capital city of Indonesia?
+Thought: I should look up Indonesia on Wikipedia.
+Action: wikipedia: Indonesia
+PAUSE
+
+You will be called again with:
+
+Observation: Indonesia,[a] officially the Republic of Indonesia,[b] is a country in Southeast Asia and Oceania between the Indian and Pacific oceans. The country's capital, Jakarta, is the world's second-most populous urban area.
+
+You will then answer, if you have the answer:
+
+Answer: The capital city of Indonesia is Jakarta.
+
+If you do not have the answer, you can return an Action and return PAUSE after.
         '''.strip()
+
         i = 0
         model.system_prompt(react_prompt)
         response = model.user_prompt(message)
